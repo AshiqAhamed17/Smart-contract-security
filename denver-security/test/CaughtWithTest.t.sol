eab7f61c6eedc7d68e435dab82bc10a2290cd94e
Ashiq Ahamed <aashiq121205@gmail.com>
2025-02-17 05:59:34 +0530

    Fixed denver-security by adding it as a normal folder

---
+++ denver-security/test/CaughtWithTest.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/CaughtWithTest.sol";
+
+contract CaughtWithTestTest is Test {
+    CaughtWithTest public caughtWithTest;
+
+    function setUp() public {
+        caughtWithTest = new CaughtWithTest();
+    }
+
+    function testSetNumber() public {
+        uint256 myNumber = 55;
+        caughtWithTest.setNumber(myNumber);
+        uint256 expectedNumber = caughtWithTest.number();
+        assertEq(myNumber ,expectedNumber, "Number mismatch");
+    }
+}
